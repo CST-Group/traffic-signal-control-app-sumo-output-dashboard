c2b069713b4dd8ba0bc361154d6ce364486efadb
Andre Paraense <andre-paraense-mobwise@users.noreply.github.com>
2016-01-31 13:41:09 -0200

    Update SumoOutputDashboard.java

--- src/br/unicamp/cogsys/ccbca/applications/sumooutputdashboard/SumoOutputDashboard.java
+++ src/br/unicamp/cogsys/ccbca/applications/sumooutputdashboard/SumoOutputDashboard.java
@@ -1,6 +1,13 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2016  DCA-FEEC-UNICAMP
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- */
+ * Contributors:
+ *     A. L. O. Paraense, R. R. Gudwin - initial implementation
+ ******************************************************************************/
 package br.unicamp.cogsys.ccbca.applications.sumooutputdashboard;
 
 import java.awt.Color;
